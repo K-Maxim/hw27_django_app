177b2fb4fd402c102d2a067870d9000658a5071b
K-Maxim <m.karpenko96@mail.ru>
2022-08-18 18:42:42 +0300

    rename http-query

--- ads/urls.py
+++ ads/urls.py
@@ -4,9 +4,10 @@ from ads import views
 urlpatterns = [
     path('cat/', views.CatView.as_view()),
     path('cat/<int:pk>', views.CatDetailView.as_view()),
+    path('cat/<int:pk>/update/', views.CatUpdateView.as_view()),
+    path('cat/<int:pk>/delete/', views.CarDeleteView.as_view()),
     path('ad/', views.AdView.as_view()),
     path('ad/<int:pk>', views.AdDetailView.as_view()),
     path('ad/<int:pk>/update/', views.AdUpdateView.as_view()),
     path('ad/<int:pk>/delete/', views.AdDeleteView.as_view()),
-
     ]
