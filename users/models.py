8fb7d3abc85f89cfafee829f10af95dbf278c2bb
K-Maxim <m.karpenko96@mail.ru>
2022-08-18 13:16:34 +0300

    update models

--- users/models.py
+++ users/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Location(models.Model):
+    name = models.CharField(max_length=100)
+    lat = models.FloatField(null=True)
+    lng = models.FloatField(null=True)
+
+    class Meta:
+        verbose_name = 'Локация'
+        verbose_name_plural = 'Локации'
+
+    def __str__(self):
+        return self.name
+
+
+class User(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50, null=True)
+    username = models.CharField(max_length=50)
+    password = models.CharField(max_length=50)
+    role = models.CharField(max_length=50)
+    age = models.IntegerField()
+    locations = models.ManyToManyField(Location)
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
+
+    def __str__(self):
+        return self.username
