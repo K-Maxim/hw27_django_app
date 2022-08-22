9ba3cba3ff6cd3c22cd02d40d1fb2859b33ba4c8
K-Maxim <m.karpenko96@mail.ru>
2022-08-22 11:24:19 +0300

    for images and pages

--- hw27_django/settings.py
+++ hw27_django/settings.py
@@ -132,4 +132,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 MEDIA_URL = "/images/"   # для картинок
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media/images')  # для картинок
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')  # для картинок
+
+TOTAL_ON_PAGE = 10
