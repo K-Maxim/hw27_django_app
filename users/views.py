e01d305e1e4c4432ffcbfb37db7cabe67d14646b
K-Maxim <m.karpenko96@mail.ru>
2022-08-28 15:59:07 +0300

    update user view

--- users/views.py
+++ users/views.py
@@ -1,16 +1,5 @@
-import json
-
-from django.core.paginator import Paginator
-from django.db.models import Count
-from django.http import JsonResponse
-from django.utils.decorators import method_decorator
-from django.views.decorators.csrf import csrf_exempt
-from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from rest_framework.generics import ListAPIView, RetrieveAPIView, CreateAPIView, UpdateAPIView, DestroyAPIView
 from rest_framework.viewsets import ModelViewSet
-
-from hw27_django import settings
-
 from users.models import User, Location
 from users.serializer import UserList_or_RetrieveSerializer, UserCreateSerializer, UserUpdateSerializer, \
     UserDestroySerializer, LocationSerializer
