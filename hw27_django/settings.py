cf4f4671f92cdfa2102ec9760d74d40ebc10dd65
K-Maxim <m.karpenko96@mail.ru>
2022-09-01 14:56:50 +0300

    for authentication

--- hw27_django/settings.py
+++ hw27_django/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework.authtoken',
+    'rest_framework_simplejwt',
     'ads',
     'users'
 ]
@@ -141,4 +143,10 @@ TOTAL_ON_PAGE = 10
 REST_FRAMEWORK = {
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
     "PAGE_SIZE": 10,  # количество вакансий на странице
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.TokenAuthentication",  # аутентификация
+        "rest_framework_simplejwt.authentication.JWTAuthentication"
+    ]
 }
+
+AUTH_USER_MODEL = 'users.User'
