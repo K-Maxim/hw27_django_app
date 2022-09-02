7214eaf8a50239c84c29191bcd0c20debead9e61
K-Maxim <m.karpenko96@mail.ru>
2022-09-02 12:11:57 +0300

    model Selection

--- ads/models.py
+++ ads/models.py
@@ -33,4 +33,14 @@ class Ad(models.Model):
         return self.name
 
 
+class Selection(models.Model):
+    name = models.CharField(max_length=50)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    items = models.ManyToManyField(Ad)
 
+    class Meta:
+        verbose_name = 'Подборка'
+        verbose_name_plural = 'Подборки'
+
+    def __str__(self):
+        return self.name
