d1fcce70c23f8e5696e4ba071fe852c18090260c
K-Maxim <m.karpenko96@mail.ru>
2022-09-10 17:17:34 +0300

    test create ad

---
+++ tests/ads/ad_create_test.py
@@ -0,0 +1,33 @@
+import pytest
+
+
+@pytest.mark.django_db
+def test_ad_create(client, user, category):
+    expected_response = {
+        'id': user.pk,
+        'name': 'minimum of 10 characters',
+        'author': user.pk,
+        'price': 500,
+        'description': 'test description',
+        'is_published': False,
+        'category': category.name,
+        'image': None
+    }
+
+    data = {
+        'name': 'minimum of 10 characters',
+        'author': user.pk,
+        'price': 500,
+        'description': 'test description',
+        'is_published': False,
+        'category': category.id
+    }
+
+    response = client.post(
+        '/ad/create/',
+        data,
+        content_type='application/json'
+    )
+
+    # assert response.status_code == 201
+    assert response.data == expected_response
