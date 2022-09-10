402c61b10930acaab729832d24cf3915a02e7f5e
K-Maxim <m.karpenko96@mail.ru>
2022-09-10 13:22:52 +0300

    register factories

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,1 +1,9 @@
+from pytest_factoryboy import register
+
+from tests.factories import AuthorFactory, CategoryFactory, AdFactory
+
 pytest_plugins = 'tests.fixtures'
+
+register(AuthorFactory)
+register(CategoryFactory)
+register(AdFactory)
